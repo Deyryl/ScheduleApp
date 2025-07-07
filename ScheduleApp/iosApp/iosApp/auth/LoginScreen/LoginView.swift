74125cdb2bb58c4d46a4aaa7ecdda12a98c8a71a
Igmnl <senior.nikitos.a@gmail.com>
2025-07-07 13:19:27 +0300

    Complete MainScreeen and created pods implementation

---
+++ ScheduleApp/iosApp/iosApp/auth/LoginScreen/LoginView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginView.swift
+//  iosApp
+//
+//  Created by Никита Арабчик on 07.07.2025.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
