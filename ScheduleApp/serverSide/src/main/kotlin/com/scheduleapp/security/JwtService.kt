446c43a453f37d67a377924f679e59d61ea7acf6
antosha-png <akkaunt9622@gmail.com>
2025-07-09 19:06:02 +0300

    Added HashEncoder & JwtService

---
+++ ScheduleApp/serverSide/src/main/kotlin/com/scheduleapp/security/JwtService.kt
@@ -0,0 +1,37 @@
+package com.scheduleapp.security
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class JwtService(@Value("JWT_SK_BASE64") private val jwtSecret: String) {
+    private val secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret))
+    private val accessTokenValidityMs = 15L * 60L * 1000L
+    val refreshTokenValidityMs = 30L * 24L * 60L * 60L * 1000L
+
+    private fun generateToken(
+        userId: String,
+        type: String,
+        expiry: Long
+    ): String {
+        val now = Date()
+        val expiryDate = Date(now.time + expiry)
+        return Jwts.builder()
+            .subject(userId)
+            .claim("type", type)
+            .issuedAt(now)
+            .expiration(expiryDate)
+            .signWith(secretKey, Jwts.SIG.HS256)
+            .compact()
+    }
+
+    fun generateAccessToken(userId: String): String {
+        return generateToken(userId, "access", accessTokenValidityMs)
+    }
+    fun generateRefreshToken(userId: String): String {
+        return generateToken(userId, "access", refreshTokenValidityMs)
+    }
+}
