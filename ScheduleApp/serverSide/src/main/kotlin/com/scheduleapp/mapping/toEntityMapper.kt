36abf7b347b27ca8a62d5ce4bb31e355451b2928
antosha-png <akkaunt9622@gmail.com>
2025-07-09 13:58:21 +0300

    Entities Updated + Project Controller

--- ScheduleApp/serverSide/src/main/kotlin/com/scheduleapp/mapping/toEntityMapper.kt
+++ ScheduleApp/serverSide/src/main/kotlin/com/scheduleapp/mapping/toEntityMapper.kt
@@ -1,43 +1,43 @@
-package com.scheduleapp.mapping
-
-import com.scheduleapp.database.models.*
-import com.vnmhpractice.scheduleapp.models.*
-import java.util.UUID
-
-
-fun User.toEntity(): UserEntity = UserEntity(
-    userID = java.util.UUID.fromString(userID),
-    hashedPassword = hashedPassword,
-    username = username,
-    email = email,
-    imageURL = imageURL,
-    projects = projects.map { it.toEntity() }
-)
-
-fun Project.toEntity(): ProjectEntity = ProjectEntity(
-    projectID = java.util.UUID.fromString(projectID),
-    title = title,
-    ownerID = UUID.fromString(owner.userID),
-    moderators = moderators.map { it.toEntity() },
-    members = members.map { it.toEntity() },
-    imageURL = imageURL,
-    //tasks = tasks.map { it.toEntity() },
-    //tags = tags.map { it.toEntity() }
-)
-
-fun Task.toEntity(): TaskEntity = TaskEntity(
-    taskID = java.util.UUID.fromString(taskID),
-    title = title,
-    description = description,
-    startTime = startTime,
-    endTime = endTime,
-    taskStatus = taskStatus,
-    tags = tags.map { it.toEntity() }
-)
-
-fun Tag.toEntity(): TagEntity = TagEntity(
-    tagID = java.util.UUID.fromString(tagID),
-    title = title,
-    description = description,
-    color = color
-)
+//package com.scheduleapp.mapping
+//
+//import com.scheduleapp.database.models.*
+//import com.vnmhpractice.scheduleapp.models.*
+//import java.util.UUID
+//
+//
+//fun User.toEntity(): UserEntity = UserEntity(
+//    userID = java.util.UUID.fromString(userID),
+//    hashedPassword = hashedPassword,
+//    username = username,
+//    email = email,
+//    imageURL = imageURL,
+//    projects = projects.map { it.toEntity() }
+//)
+//
+//fun Project.toEntity(): ProjectEntity = ProjectEntity(
+//    projectID = java.util.UUID.fromString(projectID),
+//    title = title,
+//    ownerID = UUID.fromString(owner.userID),
+//    moderators = moderators.map { it.toEntity() },
+//    members = members.map { it.toEntity() },
+//    imageURL = imageURL,
+//    //tasks = tasks.map { it.toEntity() },
+//    //tags = tags.map { it.toEntity() }
+//)
+//
+//fun Task.toEntity(): TaskEntity = TaskEntity(
+//    taskID = java.util.UUID.fromString(taskID),
+//    title = title,
+//    description = description,
+//    startTime = startTime,
+//    endTime = endTime,
+//    taskStatus = taskStatus,
+//    tags = tags.map { it.toEntity() }
+//)
+//
+//fun Tag.toEntity(): TagEntity = TagEntity(
+//    tagID = java.util.UUID.fromString(tagID),
+//    title = title,
+//    description = description,
+//    color = color
+//)
